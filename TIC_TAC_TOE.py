ed904d4593fb8e26d9cf5eaeafd62cdedd52379b
Kaleb Grimes <148815550+KalebGrimes@users.noreply.github.com>
2023-10-23 17:19:27 -0400

    Tic-Tac-Toe
    Version: Python 3.11.6

---
+++ TIC_TAC_TOE.py
@@ -0,0 +1,68 @@
+# Setting up the game board as a list
+
+board = ["-", "-", "-",
+		"-", "-", "-",
+		"-", "-", "-"]
+
+# Function to print the game board
+
+def print_board():
+	print(board[0] + " | " + board[1] + " | " + board[2])
+	print(board[3] + " | " + board[4] + " | " + board[5])
+	print(board[6] + " | " + board[7] + " | " + board[8])
+
+# Function to handle a player's turn
+
+def take_turn(player):
+	print(player + "'s turn.")
+	position = input("Choose a position from 1-9: ")
+	while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+		position = input("Invalid input. Choose a position from 1-9: ")
+	position = int(position) - 1
+	while board[position] != "-":
+		position = int(input("Position already taken. Choose a different position: ")) - 1
+	board[position] = player
+	print_board()
+
+# Game-state checker
+
+def check_game_over():
+	# Check for a win
+
+	if (board[0] == board[1] == board[2] != "-") or \
+	   (board[3] == board[4] == board[5] != "-") or \
+	   (board[6] == board[7] == board[8] != "-") or \
+	   (board[0] == board[3] == board[6] != "-") or \
+	   (board[1] == board[4] == board[7] != "-") or \
+	   (board[2] == board[5] == board[8] != "-") or \
+	   (board[0] == board[4] == board[8] != "-") or \
+	   (board[2] == board[4] == board[6] != "-"):
+		return "win"
+	# Check for a tie
+	elif "-" not in board:
+		return "tie"
+	# Game is not over then-
+	else:
+		return "play"
+
+# Main game
+def play_game():
+	print_board()
+	current_player = "X"
+	game_over = False
+	
+	while not game_over:
+		take_turn(current_player)
+		game_result = check_game_over()
+		if game_result == "win":
+			print(current_player + " wins!")
+			game_over = True
+		elif game_result == "tie":
+			print("It's a tie!")
+			game_over = True
+		else:
+			# Switch to the other player
+			current_player = "O" if current_player == "X" else "X"
+
+# Start the game
+play_game()
